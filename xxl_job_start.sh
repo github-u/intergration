d00e70186ae02e76b3dab36c8ea11287365f5f54
suxiong.sx <suxiong.sx@alibaba-inc.com>
2020-01-20 14:48:03 +0800

    Commit By Git-Helper

--- xxl_job_start.sh
+++ xxl_job_start.sh
@@ -23,4 +23,4 @@ cp xxl-job-admin/target/xxl-job-admin-2.2.0-SNAPSHOT.jar ~/task
 
 echo "start app ..."
 cd ~/task
-java -jar xxl-job-admin-2.2.0-SNAPSHOT.jar
+java -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=64m -Xms384m -Xmx384m -Xmn384m -Xss256k -XX:SurvivorRatio=8 -Xloggc:gc.log:wq  -jar xxl-job-admin-2.2.0-SNAPSHOT.jar
